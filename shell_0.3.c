1809c8048faaec4d182f738e64e4566736e0df59
Melanie Saina <Gabbysiana@gmail.com>
2023-04-23 01:56:57 -0700

    03

--- shell_0.3.c
+++ shell_0.3.c
@@ -1,176 +1,22 @@
 #include "main.h"
 
-extern char **environ;
-int main4(void)
+int main(void)
 {
     char cmd[MAX_CMD_LEN];
     char *args[MAX_ARGS];
     int status, i;
     char *token;
-    char *path, *path_token, *path_copy;
-    char full_path[MAX_CMD_LEN];
-	char *new_path = "/usr/bin:/usr/local/bin"; /* add the directories containing the commands you want to use*/
-	setenv("PATH", new_path, 1);
-    while (1)
-    {
-        printf(PROMPT);
-        if (fgets(cmd, MAX_CMD_LEN, stdin) == NULL)
-        {
-            printf("\n");
-            exit(0);
-        }
-        cmd[strlen(cmd) - 1] = '\0';
 
-        i = 0;
-        token = strtok(cmd, DELIM);
-        while (token != NULL)
-        {
-            args[i] = token;
-            i++;
-            token = strtok(NULL, DELIM);
-        }
-        args[i] = NULL;
+    char* system_path = getenv("PATH");
+    char* updated_path = malloc(strlen(system_path) + strlen(getenv("PATH")) + 2);
 
-        if (args[0] != NULL)
-        {
-            /* Check for built-in commands*/
-            if (strcmp(args[0], "exit") == 0)
-            {
-                exit(0);
-            }
+    strcpy(updated_path, system_path);
+    strcat(updated_path, ":");
+    strcat(updated_path, getenv("PATH"));
 
-            path = _getenv("PATH");
-            path_copy = strdup(path);
-            path_token = strtok(path_copy, DELIM);
-            while (path_token != NULL)
-            {
-                _strcpy(full_path, path_token);
-                _strcat(full_path, "/");
-                _strcat(full_path, args[0]);
+    setenv("PATH", updated_path, 1);
 
-                if (access(full_path, F_OK) == 0)
-                {
-                    if (fork() == 0)
-                    {
-                        execve(full_path, args, NULL);
-                    }
-                    else
-                    {
-                        wait(&status);
-                        break;
-                    }
-                }
-
-                path_token = strtok(NULL, DELIM);
-            }
-
-            if (path_token == NULL)
-            {
-                printf("%s: command not found\n", args[0]);
-            }
-
-            free(path_copy);
-        }
-    }
-
-    return (0);
-}
-char *_getenv(const char *name)
-{
-        int i, len;
-        len = _strlen(name);
-        for (i = 0; environ[i] != NULL; i++)
-        {
-                if (_strncmp(environ[i], name, len) == 0 && environ[i][len] == '=')
-                {
-                        return (environ[i] + len + 1);
-                }
-        }
-        return (NULL);
-}
-char *_strcat(char *dest, char *src)
-{
-    int dest_len = _strlen(dest);
-    int i;
-
-    for (i = 0; src[i] != '\0'; i++)
-    {
-        dest[dest_len + i] = src[i];
-    }
-    dest[dest_len + i] = '\0';
-
-    return (dest);
-}
-
-int _strcmp(char *s1, char *s2)
-{
-    int i;
-
-    for (i = 0; s1[i] == s2[i]; i++)
-    {
-        if (s1[i] == '\0')
-        {
-            return (0);
-        }
-    }
-
-    return (s1[i] - s2[i]);
-}
-int _strncmp(const char*s1, const char *s2, int n)
-{
-    int i;
-
-    for (i = 0; i < n && s1[i] == s2[i]; i++)
-    {
-        if (s1[i] == '\0')
-        {
-            return (0);
-        }
-    }
-
-    if (i == n)
-    {
-        return (0);
-    }
-
-    return (s1[i] - s2[i]);
-}
-
-char *_strcpy(char *dest, char *src)
-{
-    int i = 0;
-
-    while (src[i] != '\0')
-    {
-        dest[i] = src[i];
-        i++;
-    }
-    dest[i] = '\0';
-
-    return (dest);
-}
-
-int _strlen(const char *s)
-{
-    int len = 0;
-
-    while (s[len] != '\0')
-    {
-        len++;
-    }
-
-    return (len);
-}
-
-int _main(void)
-{
-    char cmd[MAX_CMD_LEN];
-    char *args[MAX_ARGS];
-    int status, i;
-    char *token;
-    char *path, *path_token, *path_copy;
-    char full_path[MAX_CMD_LEN];
-    int cmd_exists = 0;
+    free(updated_path);
 
     while (1)
     {
@@ -180,11 +26,11 @@ int _main(void)
             printf("\n");
             exit(0);
         }
-        cmd[strlen(cmd) - 1] = '\0';
+        cmd[strcspn(cmd, "\n")] = '\0';
 
         i = 0;
         token = strtok(cmd, DELIM);
-        while (token != NULL)
+        while (token != NULL && i < MAX_ARGS-1)
         {
             args[i] = token;
             i++;
@@ -192,43 +38,58 @@ int _main(void)
         }
         args[i] = NULL;
 
-        if (args[0] != NULL)
+        if (i > 0 && strcmp(args[0], "exit") == 0)
         {
-            path = _getenv("PATH");
-            path_copy = strdup(path);
-            path_token = strtok(path_copy, DELIM);
-            while (path_token != NULL)
+            exit(0);
+        }
+
+        if (access(args[0], X_OK) == 0)
+        {
+            if (fork() == 0)
             {
-                _strcpy(full_path, path_token);
-                _strcat(full_path, "/");
-                _strcat(full_path, args[0]);
-
-                if (access(full_path, F_OK) == 0)
+                if (execvp(args[0], args) == -1)
                 {
-                    cmd_exists = 1;
-
+                    perror(args[0]);
+                    exit(0);
+                }
+            }
+            else
+            {
+                wait(&status);
+            }
+        }
+        else
+        {
+            char *path = getenv("PATH");
+            char *dir = strtok(path, ":");
+            while (dir != NULL)
+            {
+                char command[MAX_CMD_LEN];
+                snprintf(command, sizeof(command), "%s/%s", dir, args[0]);
+                if (access(command, X_OK) == 0)
+                {
+                    args[0] = command;
                     if (fork() == 0)
                     {
-                        execve(full_path, args, NULL);
+                        if (execvp(args[0], args) == -1)
+                        {
+                            perror(args[0]);
+                            exit(0);
+                        }
                     }
                     else
                     {
                         wait(&status);
-                        break;
                     }
+                    break;
                 }
-
-                path_token = strtok(NULL, DELIM);
+                dir = strtok(NULL, ":");
             }
-
-            if (!cmd_exists)
+            if (dir == NULL)
             {
-                printf("%s: command not found\n", args[0]);
+                fprintf(stderr, "%s: command not found\n", args[0]);
             }
-
-            free(path_copy);
         }
     }
-
     return (0);
 }
